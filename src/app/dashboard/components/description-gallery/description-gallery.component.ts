42c4126cd86b95d9b503fe0ac087f337da4a2c0a
Aleksey Zolotarev <aleksey.zolotarev@logmein.com>
2019-03-19 10:37:53 +0200

    added data

--- src/app/dashboard/components/description-gallery/description-gallery.component.ts
+++ src/app/dashboard/components/description-gallery/description-gallery.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, Output } from '@angular/core';
 
 @Component({
   selector: 'kt-description-gallery',
@@ -6,10 +6,55 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   styleUrls: ['./description-gallery.component.scss']
 })
 
-export class DescriptionGalleryComponent {
+export class DescriptionGalleryComponent implements OnChanges {
   @Input() data: any[] = [];
+
   @Input() currentIndex = 0;
+
   @Output() switchSlide: EventEmitter<number> = new EventEmitter();
+
+  public readMoreData: any = [];
+
+  private _maxAllowedCharacters = 300;
+
+  ngOnChanges() {
+    this.defineReadMoreData();
+  }
+
+  defineReadMoreData(): void {
+    this.readMoreData = [];
+
+    for (let i = 0; i < this.data.length; i++) {
+      let characterCount = this._maxAllowedCharacters;
+      this.readMoreData.push({
+        content: []
+      });
+
+      for (const text of this.data[i].content) {
+        const count = Math.min(characterCount, text.length);
+        const cutText = text.substring(0, count);
+
+        characterCount -= count;
+        if (characterCount === 0) {
+          if (
+            cutText.length < text.length ||
+            this.readMoreData[i].content.length < this.readMoreData[i].content.length
+          ) {
+            this.readMoreData[i].readMoreActive = true;
+          }
+          this.readMoreData[i].content.push(cutText + '...');
+          break;
+        } else {
+          this.readMoreData[i].content.push(cutText);
+        }
+      }
+    }
+  }
+
+  toggleSize(e: MouseEvent, item): void {
+    e.preventDefault();
+    item.showAll = !item.showAll;
+  }
 
   nextSlide(e: MouseEvent): void {
     if (e) {
